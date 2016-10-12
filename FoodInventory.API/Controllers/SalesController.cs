9c24059c1232a76102329dea33da2d74159a10f7
Krishna Kolluru <Krishna Kolluru>
2016-10-12 02:57:08 -0400

    FirstCommit

---
+++ FoodInventory.API/Controllers/SalesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FoodInventory.Data;
+using FoodInventory.Data.Models;
+
+namespace FoodInventory.API.Controllers
+{
+    [RoutePrefix("api/Sales")]
+    public class SalesController : ApiController
+    {
+        private UnitOfWork _unitOfWork = new UnitOfWork();
+
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] FoodInventory.Data.Models.DTOs.SalesDTO productSold)
+        {
+            try
+            {
+                var messageToReturn = "";
+                var productToEdit = _unitOfWork.ProductRepository.Get().Where(p => p.ID == productSold.ID).FirstOrDefault();
+                if (productToEdit == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "The product (" + productSold.Name + ") with ID (" + productSold.ID + ") does not exist.");
+                }
+                else
+                {
+                    //Edit this product.
+                    productSold.NumberOfUnits = productSold.NumberOfUnits;
+                    productSold.SaleDate = DateTime.Now;
+                    productSold.PaymentType = productSold.PaymentType;
+                    _unitOfWork.SalesRepository.Sell(productSold);
+                    _unitOfWork.Save();
+                    messageToReturn = "Item Sold (" + productSold.Name + ") with ID (" + productSold.ID + ").";
+                }
+
+
+                return Request.CreateResponse(HttpStatusCode.OK, messageToReturn);
+            }
+            catch (Exception exc)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, exc.ToString());
+            }
+        }
+
+    }
+}
---
+++ FoodInventory.Data/Models/DTOs/SalesDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodInventory.Data.Models.DTOs
+{
+    public class SalesDTO
+    {
+        public int ID { get; set; }
+        public Nullable<System.DateTime> SaleDate { get; set; }
+        public string Name { get; set; }
+        public decimal NumberOfUnits { get; set; }
+        public decimal Discount { get; set; }
+        public string PaymentType { get; set; }
+
+    }
+}
